8b43895b3006adb214ead7115eb8190298e37e27
Keith Hughitt <keith.hughitt@gmail.com>
2015-12-21 20:30:46 -0500

    Added support for L. braziliensis

--- build_orgdb.R
+++ build_orgdb.R
@@ -20,9 +20,9 @@ options(stringsAsFactors=FALSE)
 # MAIN
 #
 # Load settings
-settings = yaml.load_file("config.yaml")
+settings <- yaml.load_file("config.yaml")
 
-build_dir = file.path(settings$build_dir,
+build_dir <- file.path(settings$build_dir,
                       paste0(R.Version()$major,  '.', R.Version()$minor))
 
 
@@ -33,26 +33,26 @@ if (!file.exists(build_dir)) {
 if (!file.exists(settings$output_dir)) {
     dir.create(settings$output_dir, recursive=TRUE)
 }
-build_basename = file.path(build_dir,
+build_basename <- file.path(build_dir,
                            sub('.gff', '', basename(settings$gff)))
 
 # Parse GFF
-gff = import.gff3(settings$gff)
-genes = gff[gff$type == 'gene']
+gff <- import.gff3(settings$gff)
+genes <- gff[gff$type == 'gene']
 
 #
 # 1. Gene name and description
 #
-gene_file = sprintf("%s_gene_info.txt", build_basename)
+gene_file <- sprintf("%s_gene_info.txt", build_basename)
 
 if (file.exists(gene_file)) {
-    gene_info = read.delim(gene_file)
+    gene_info <- read.delim(gene_file)
 } else {
     print("Parsing gene information...")
-    gene_info = as.data.frame(elementMetadata(genes[,c('ID', 'description')]))
+    gene_info <- as.data.frame(elementMetadata(genes[,c('ID', 'description')]))
 
     # Convert form-encoded description string to human-readable
-    gene_info$description = gsub("\\+", " ", gene_info$description)
+    gene_info$description <- gsub("\\+", " ", gene_info$description)
     colnames(gene_info) <- c("GID", "GENENAME")
 
     write.table(gene_info, gene_file, sep='\t', quote=FALSE, row.names=FALSE)
@@ -61,13 +61,13 @@ if (file.exists(gene_file)) {
 #
 # 2. Chromosome information
 #
-chr_file = sprintf("%s_chr_info.txt", build_basename)
+chr_file <- sprintf("%s_chr_info.txt", build_basename)
 
 if (file.exists(chr_file)) {
-    chr_info = read.delim(chr_file)
+    chr_info <- read.delim(chr_file)
 } else {
     print("Parsing chromosome information...")
-    chr_info = data.frame(
+    chr_info <- data.frame(
         'GID' = genes$ID,
         'CHR' = as.character(seqnames(genes))
     )
@@ -77,50 +77,50 @@ if (file.exists(chr_file)) {
 #
 # 3. Gene type information
 #
-gene_type_file = sprintf("%s_gene_type.txt", build_basename)
+gene_type_file <- sprintf("%s_gene_type.txt", build_basename)
 
 if (file.exists(gene_type_file)) {
-    gene_types = read.delim(gene_type_file)
+    gene_types <- read.delim(gene_type_file)
 } else {
     print("Parsing gene types...")
-    gene_types = parse_gene_types(settings$txt)
+    gene_types <- parse_gene_types(settings$txt)
     write.table(gene_types, gene_type_file, sep='\t', quote=FALSE, row.names=FALSE)
 }
 
 #
 # 4. Gene ontology information
 #
-go_file = sprintf("%s_go_table.txt", build_basename)
+go_file <- sprintf("%s_go_table.txt", build_basename)
 
 if (file.exists(go_file)) {
-    go_table = read.delim(go_file)
+    go_table <- read.delim(go_file)
 } else {
     library('GO.db')
 
     print("Parsing GO annotations...")
-    go_table = parse_go_terms(settings$txt)
+    go_table <- parse_go_terms(settings$txt)
 
     # Map from non-primary IDs to primary GO ids;
     # non-primary IDs are filtered out by makeOrgPackage
-    problem_rows = go_table[!go_table$GO %in% keys(GO.db),]
-    synonyms = problem_rows$GO
+    problem_rows <- go_table[!go_table$GO %in% keys(GO.db),]
+    synonyms <- problem_rows$GO
 
     # Create a mapping data frame
-    synonym_mapping = data.frame()
+    synonym_mapping <- data.frame()
 
     for (syn in synonyms) {
         if (!is.null(GOSYNONYM[[syn]])) {
-            synonym_mapping = rbind(synonym_mapping, c(syn, GOSYNONYM[[syn]]@GOID))
+            synonym_mapping <- rbind(synonym_mapping, c(syn, GOSYNONYM[[syn]]@GOID))
         }
     }
     
     # replace alternative GO term identifiers
     if (nrow(synonym_mapping) > 0) {
-        colnames(synonym_mapping) = c('synonym', 'primary')
-        synonym_mapping = unique(synonym_mapping)
+        colnames(synonym_mapping) <- c('synonym', 'primary')
+        synonym_mapping <- unique(synonym_mapping)
 
-        go_table$GO[!go_table$GO %in% keys(GO.db)] = synonym_mapping$primary[match(synonyms, synonym_mapping$synonym)]
-        go_table = unique(go_table[complete.cases(go_table),])
+        go_table$GO[!go_table$GO %in% keys(GO.db)] <- synonym_mapping$primary[match(synonyms, synonym_mapping$synonym)]
+        go_table <- unique(go_table[complete.cases(go_table),])
     }
 
     write.table(go_table, go_file, sep='\t', quote=FALSE, row.names=FALSE)
@@ -129,38 +129,42 @@ if (file.exists(go_file)) {
 #
 # 5. KEGG information
 #
-kegg_mapping_file  = sprintf("%s_kegg_mapping.txt", build_basename)
-kegg_pathways_file = sprintf("%s_kegg_pathways.txt", build_basename)
+kegg_mapping_file  <- sprintf("%s_kegg_mapping.txt", build_basename)
+kegg_pathways_file <- sprintf("%s_kegg_pathways.txt", build_basename)
 
-convert_kegg_gene_ids = function(kegg_ids, kegg_id_mapping) {
-    result = c()
+convert_kegg_gene_ids <- function(kegg_ids, kegg_id_mapping) {
+    result <- c()
     for (kegg_id in kegg_ids) {
         if (substring(kegg_id, 1, 4) == 'tbr:') {
             # T. brucei
-            result = append(result,
+            result <- append(result,
                 gsub('tbr:', '', kegg_id))
         } else if (substring(kegg_id, 1, 4) == 'tcr:') {
             # T. cruzi
-            result = append(result,
+            result <- append(result,
                 gsub('tcr:', 'TcCLB.', kegg_id))
         } else if (substring(kegg_id, 1, 4) == 'tgo:') {
             # T. gondii
-            result = append(result, gsub('tgo:', '', gsub('_', '.', kegg_id)))
+            result <- append(result, gsub('tgo:', '', gsub('_', '.', kegg_id)))
+        } else if (substring(kegg_id, 1, 9) == 'lbz:LBRM_') {
+            # L. braziliensis (lbz:LBRM_01_0080)
+            result <- append(result, gsub('LBRM', 'LbrM', 
+                     gsub("_", "\\.", substring(kegg_id, 5))))
         } else if (substring(kegg_id, 1, 9) == 'lma:LMJF_') {
             # L. major (lma:LMJF_11_0100)
-            result = append(result, 
+            result <- append(result, 
                 gsub('LMJF', 'LmjF', 
                      gsub("_", "\\.", substring(kegg_id, 5))))
         } else if (substring(kegg_id, 1, 8) == 'lma:LMJF') {
             # L. major (lma:LMJF10_TRNALYS_01)
-            parts = unlist(strsplit(kegg_id, "_"))
-            result = append(result,
+            parts <- unlist(strsplit(kegg_id, "_"))
+            result <- append(result,
                 sprintf("LmjF.%s.%s.%s",
                         substring(kegg_id, 9, 10),
                         parts[2], parts[3]))
         } else {
             print(sprintf("Skipping KEGG id: %s", kegg_id))
-            result = append(result, NA)
+            result <- append(result, NA)
         }
     }
     return(result)
@@ -170,8 +174,17 @@ if (!file.exists(kegg_mapping_file)) {
     library(KEGGREST)
 
     # KEGG Organism abbreviation (e.g. "lma")
-    org_abbreviation = paste0(tolower(substring(settings$genus, 1, 1)), 
+    org_abbreviation <- paste0(tolower(substring(settings$genus, 1, 1)), 
                               substring(settings$species, 1, 2))
+
+    # Overides for cases where KEGG abbreviation differes from the above
+    # pattern.
+
+    # L. braziliensis
+    if (org_abbreviation == 'lbr') {
+        org_abbreviation <- 'lbz'
+    }
+
 
 
     # For some species, it is necessary to map gene ids from KEGG to what is
@@ -182,11 +195,11 @@ if (!file.exists(kegg_mapping_file)) {
     if (org_abbreviation == 'tbr') {
         # Load GeneAlias file and convert entry in KEGG results
         # to newer GeneDB/TriTrypDB identifiers.
-        fp = file(settings$aliases)
-        rows = strsplit(readLines(fp), "\t")
+        fp <- file(settings$aliases)
+        rows <- strsplit(readLines(fp), "\t")
         close(fp)
 
-        kegg_id_mapping = list()
+        kegg_id_mapping <- list()
 
         # example alias file entries
         #Tb927.10.2410  TRYP_x-70a06.p2kb545_720  Tb10.70.5290
@@ -196,10 +209,10 @@ if (!file.exists(kegg_mapping_file)) {
         #Tb927.11.7240  Tb11.02.5150  Tb11.02.5150:mRNA  Tb11.02.5150:pep
         for (row in rows) {
             # get first and third columns in the alias file
-            old_ids = row[2:length(row)]
+            old_ids <- row[2:length(row)]
 
             for (old_id in old_ids[grepl('Tb\\d+\\.\\w+\\.\\d+', old_ids)]) {
-                kegg_id_mapping[old_id] = row[1]
+                kegg_id_mapping[old_id] <- row[1]
             }
         }
     
@@ -212,63 +225,63 @@ if (!file.exists(kegg_mapping_file)) {
         # format, e.g. "md:lma_M00359", and "bsid:85066"
         #
     } else if (org_abbreviation == 'tcr') {
-        fp = file(settings$aliases)
-        rows = strsplit(readLines(fp), "\t")
+        fp <- file(settings$aliases)
+        rows <- strsplit(readLines(fp), "\t")
         close(fp)
 
-        kegg_id_mapping = list()
+        kegg_id_mapping <- list()
 
         for (row in rows) {
             # get first and third columns in the alias file
-            kegg_id_mapping[row[3]] = row[1]
+            kegg_id_mapping[row[3]] <- row[1]
         }
 
         # Example: "tcr:509463.30" -> ""
-        ##convert_kegg_gene_ids = function(kegg_ids) {
+        ##convert_kegg_gene_ids <- function(kegg_ids) {
         ##    kegg_to_genedb(kegg_ids, kegg_id_mapping)   
         ##}
     }
 
     # data frame to store kegg gene mapping and pathway information
-    kegg_mapping = data.frame()
-    kegg_pathways = data.frame()
+    kegg_mapping <- data.frame()
+    kegg_pathways <- data.frame()
 
-    pathways = unique(keggLink("pathway", org_abbreviation))
+    pathways <- unique(keggLink("pathway", org_abbreviation))
 
     # Iterate over pathways and query genes for each one
     for (pathway in pathways) {
         message(sprintf("Processing genes for KEGG pathway %s", pathway))
 
         # Get pathway info
-        meta = keggGet(pathway)[[1]]
-        pathway_desc  = ifelse(is.null(meta$DESCRIPTION), '', meta$DESCRIPTION)
-        pathway_class = ifelse(is.null(meta$CLASS), '', meta$CLASS)
-        kegg_pathways = rbind(kegg_pathways, 
-                              data.frame(pathway=pathway,
+        meta <- keggGet(pathway)[[1]]
+        pathway_desc  <- ifelse(is.null(meta$DESCRIPTION), '', meta$DESCRIPTION)
+        pathway_class <- ifelse(is.null(meta$CLASS), '', meta$CLASS)
+        kegg_pathways <- rbind(kegg_pathways, 
+                               data.frame(pathway=pathway,
                                          name=meta$PATHWAY_MAP,
                                          class=pathway_class,
                                          description=pathway_desc))
         
         # Get genes in pathway
-        kegg_ids = as.character(keggLink(org_abbreviation, pathway))
-        gene_ids = convert_kegg_gene_ids(kegg_ids)
+        kegg_ids <- as.character(keggLink(org_abbreviation, pathway))
+        gene_ids <- convert_kegg_gene_ids(kegg_ids)
 
         # Map old T. brucei gene names
         if (org_abbreviation == 'tbr') {
-            old_gene_ids = gene_ids
-            gene_ids = c()
+            old_gene_ids <- gene_ids
+            gene_ids <- c()
 
             for (x in old_gene_ids) {
                 if (x %in% names(kegg_id_mapping)) {
-                    gene_ids = append(gene_ids, kegg_id_mapping[[x]])              
+                    gene_ids <- append(gene_ids, kegg_id_mapping[[x]])              
                 } else {
-                    gene_ids = append(gene_ids, x)              
+                    gene_ids <- append(gene_ids, x)              
                 }
             }
         }
 
         if (!is.null(gene_ids)) {
-            kegg_mapping = unique(rbind(kegg_mapping,
+            kegg_mapping <- unique(rbind(kegg_mapping,
                 data.frame(GID=gene_ids, pathway=pathway)))
         }
     }
@@ -279,23 +292,23 @@ if (!file.exists(kegg_mapping_file)) {
               row.names=FALSE, sep='\t')
 } else {
     # Otherwise load saved version
-    kegg_mapping = read.csv(kegg_mapping_file)
-    kegg_pathways = read.delim(kegg_pathways_file)
+    kegg_mapping <- read.csv(kegg_mapping_file)
+    kegg_pathways <- read.delim(kegg_pathways_file)
 }
 
 # Drop columns with unrecognized identifiers
-kegg_mapping = kegg_mapping[complete.cases(kegg_mapping),]
+kegg_mapping <- kegg_mapping[complete.cases(kegg_mapping),]
 
 # Combined KEGG table
-kegg_table = merge(kegg_mapping, kegg_pathways, by='pathway')
-colnames(kegg_table) = c("KEGG_PATH", "GID", "KEGG_NAME", "KEGG_CLASS",
+kegg_table <- merge(kegg_mapping, kegg_pathways, by='pathway')
+colnames(kegg_table) <- c("KEGG_PATH", "GID", "KEGG_NAME", "KEGG_CLASS",
                          "KEGG_DESCRIPTION")
 
 # reorder so GID comes first
-kegg_table = kegg_table[,c(2, 1, 3, 4, 5)]
+kegg_table <- kegg_table[,c(2, 1, 3, 4, 5)]
 
 # R package versions must be of the form "x.y"
-db_version = paste(settings$db_version, '0', sep='.')
+db_version <- paste(settings$db_version, '0', sep='.')
 
 # Generate package
 makeOrgPackage(
